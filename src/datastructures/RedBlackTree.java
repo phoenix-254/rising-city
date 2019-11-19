18966a4641fd51503f1395745c93763ba4ef9307
shaishav <shaishav.shah.ce@gmail.com>
2019-11-19 00:56:08 -0500

    Implemented red-black-tree deletion

--- src/datastructures/RedBlackTree.java
+++ src/datastructures/RedBlackTree.java
@@ -1,235 +1,277 @@
 package datastructures;
 
-import java.util.Stack;
-
 import pojo.*;
 
 public class RedBlackTree {
-    // Number of currently inserted items in the red black tree.
-    private int size;
-
     // Root node of the red black tree.
     private RedBlackNode root;
 
-    public RedBlackTree() {}
+    // Null external black node.
+    private RedBlackNode nil;
 
-    // Adds node into the tree.
-    // Node is added as per binary search tree rules, and then rebalanced to maintain red black tree property.
-    public void add(RedBlackNode node) {
-        if(node == null) 
-            throw new IllegalArgumentException("Error: Invalid input. Cannot insert null.");
-
-        // Initialized root node.
-        if(root == null) {
-            root = node;
-            root.setColor(NodeColor.BLACK);
-            size++;
-            return;
-        }
-
-        // Create a copy of root node.
-        RedBlackNode rootCopy = root;
-        while(rootCopy != null) {
-            // Generate error and stop if building already exists.
-            if(rootCopy.getBuildingNumber() == node.getBuildingNumber()) {
-                throw new IllegalArgumentException("Error. Trying to insert duplicate node. Building with building id " 
-                + node.getBuildingNumber() + " already exists.");
-            }
-
-            // If root value is greater than the node to be inserted, i.e insert into left sub-tree.
-            if(rootCopy.getBuildingNumber() > node.getBuildingNumber()) {
-                // Add node as a left child if it's null.
-                if(rootCopy.getLeft() == null) {
-                    rootCopy.setLeft(node);
-                    rootCopy.getLeft().setParent(rootCopy);
-                    rebalance(rootCopy.getLeft());
-                    size++;
-                    return;
-                }
-                // Update root and continue.
-                rootCopy = rootCopy.getLeft();
-            }
-            // insert into right-subtree.
-            else {
-                // Add node as a right child if it's null.
-                if(rootCopy.getRight() == null) {
-                    rootCopy.setRight(node);
-                    rootCopy.getRight().setParent(rootCopy);
-                    rebalance(rootCopy.getRight());
-                    size++;
-                    return;
-                }
-                // Update root and continue.
-                rootCopy = rootCopy.getRight();
-            }
-        }
+    // Constructor.
+    public RedBlackTree() {
+        this.nil = RedBlackNode.NIL;
+        this.root = RedBlackNode.NIL;
     }
 
-
-    private void rebalance(RedBlackNode node) {
-        RedBlackNode parent = node.getParent();
-        // If root node, nothing to do.
-        if(parent == null) return;
-        
-        RedBlackNode grandParent = parent.getParent();
-        if(grandParent == null) return;
-
-        // Already balanced, since there are no consecutive red nodes.
-        if(node.getColor() == NodeColor.BLACK || parent.getColor() == NodeColor.BLACK) {
-            return;
-        }
-
-        boolean isNodeLeft = (parent.getLeft() == node);
-        boolean isParentLeft = (grandParent.getLeft() == parent);
-
-        RedBlackNode uncle = isParentLeft ? grandParent.getRight() : grandParent.getLeft();
-        boolean isUncleRed = (uncle != null ? (uncle.getColor() == NodeColor.RED) : false);
-
-        // If uncle node is red, perform recoloring.
-        if(isUncleRed) {
-            if(grandParent.getParent() != null) grandParent.setColor(NodeColor.RED);
-            parent.setColor(NodeColor.BLACK);
-            uncle.setColor(NodeColor.BLACK);
-            // Continue rebalancing from grandparent.
-            rebalance(grandParent);
-        }
-        // If uncle node is black, perform rotations.
-        else {
-            if(isParentLeft) {
-                if(isNodeLeft) {
-                    // LLb rotation
-                    leftLeftBlackCase(node, parent, grandParent);
-                } else {
-                    // LRb rotation
-                    leftRightBlackCase(node, parent, grandParent);
-                }
-            } else {
-                if(isNodeLeft) {
-                    // RLb rotation
-                    rightLeftBlackCase(node, parent, grandParent);
-                } else {
-                    // RRb rotation
-                    rightRightBlackCase(node, parent, grandParent);
-                }
-            }
-        }
+    // Checks if the node is left child.
+    private boolean isLeftChild(RedBlackNode node) {
+        return (node == node.getParent().getLeft());
     }
 
-    // Performs LL rotation to rebalance the tree.
-    private void leftLeftBlackCase(RedBlackNode node, RedBlackNode parent, RedBlackNode grandParent) {
-        RedBlackNode temp = parent.getRight();
-        RedBlackNode parentOfGrandParent = grandParent.getParent();
-
-        grandParent.setColor(NodeColor.RED);
-        grandParent.setLeft(temp);
-        if(temp != null) temp.setParent(grandParent);
-
-        parent.setRight(grandParent);
-        parent.setColor(NodeColor.BLACK);
-        parent.setParent(parentOfGrandParent);
-        grandParent.setParent(parent);
-
-        updateParentChildLink(parentOfGrandParent, grandParent, parent);
-    }
-
-    // Performs RR rotation to rebalance the tree.
-    private void rightRightBlackCase(RedBlackNode node, RedBlackNode parent, RedBlackNode grandParent) {
-        RedBlackNode temp = parent.getLeft();
-        RedBlackNode parentOfGrandParent = grandParent.getParent();
-
-        grandParent.setColor(NodeColor.RED);
-        grandParent.setRight(temp);
-        if(temp != null) temp.setParent(grandParent);
-
-        parent.setLeft(grandParent);
-        parent.setColor(NodeColor.BLACK);
-        parent.setParent(parentOfGrandParent);
-        grandParent.setParent(parent);
-
-        updateParentChildLink(parentOfGrandParent, grandParent, parent);
-    }
-
-    // Performs LR rotation to rebalance the tree.
-    private void leftRightBlackCase(RedBlackNode node, RedBlackNode parent, RedBlackNode grandParent) {
-        RedBlackNode left = node.getLeft();
-        RedBlackNode right = node.getRight();
-        RedBlackNode parentOfGrandParent = grandParent.getParent();
-
-        parent.setRight(left);
-        if(left != null) left.setParent(parent);
-
-        grandParent.setLeft(right);
-        if(right != null) right.setParent(grandParent);
-
-        node.setLeft(parent);
-        node.setRight(grandParent);
-        node.setParent(parentOfGrandParent);
-        node.setColor(NodeColor.BLACK);
-        
-        grandParent.setParent(node);
-        grandParent.setColor(NodeColor.RED);
-
-        updateParentChildLink(parentOfGrandParent, grandParent, node);
-    }
-
-    // Performs RL rotation to rebalance the tree.
-    private void rightLeftBlackCase(RedBlackNode node, RedBlackNode parent, RedBlackNode grandParent) {
-        RedBlackNode left = node.getLeft();
-        RedBlackNode right = node.getRight();
-        RedBlackNode parentOfGrandParent = grandParent.getParent();
-        
-        parent.setLeft(right);
-        if(right != null) right.setParent(parent);
-
-        grandParent.setRight(left);
-        if(left != null) left.setParent(grandParent);
-
-        node.setLeft(grandParent);
-        node.setRight(parent);
-        node.setParent(parentOfGrandParent);
-        node.setColor(NodeColor.BLACK);
-
-        grandParent.setParent(node);
-        grandParent.setColor(NodeColor.RED);
-
-        updateParentChildLink(parentOfGrandParent, grandParent, node);
+    // Checks if the node is right child.
+    private boolean isRightChild(RedBlackNode node) {
+        return (node == node.getParent().getRight());
     }
 
     // Updates child link for parent node.
     private void updateParentChildLink(RedBlackNode parent, RedBlackNode oldChild, RedBlackNode newChild) {
-        if(parent != null) {
-            // If the old child was in the left.
-            if(parent.getLeft() == oldChild) {
+        if(parent != nil) {
+            if(isLeftChild(oldChild)) { // If the old child was in the left.
                 parent.setLeft(newChild);
-            }
-            // If the old child was in the right.
-            else {
+            } else { // If the old child was in the right.
                 parent.setRight(newChild);
             }
-        } else {
-            // If rotation caused the new child to become root.
+        } else { // If rotation caused the new child to become root.
             root = newChild;
+        }
+
+        // Set parent as new-child's parent.
+        newChild.setParent(parent);
+    }
+
+    // Peforms left rotation for node.
+    private void rotateLeft(RedBlackNode node) {
+        // Copy node's right child.
+        RedBlackNode yNode = node.getRight();
+        
+        // Update node's right child by yNode's left subtree.
+        node.setRight(yNode.getLeft());
+
+        // Update yNode's left child's parent by node if it's not a leaf node.
+        if(yNode.getLeft() != nil) {
+            yNode.getLeft().setParent(node);
+        }
+
+        updateParentChildLink(node.getParent(), node, yNode);
+
+        yNode.setLeft(node);
+        node.setParent(yNode);
+    }
+
+    // Performs right rotation for the node.
+    private void rotateRight(RedBlackNode node) {
+        // Copy node's left child.
+        RedBlackNode yNode = node.getLeft();
+
+        // Update node's left child by yNode's right subtree.
+        node.setLeft(yNode.getRight());
+
+        // Update yNode's right child's parent by node if it's not a leaf node.
+        if(yNode.getRight() != nil) {
+            yNode.getRight().setParent(node);
+        }
+
+        updateParentChildLink(node.getParent(), node, yNode);
+
+        yNode.setRight(node);
+        node.setParent(yNode);
+    }
+
+    // Adds node into the tree.
+    // Node is added as per binary search tree rules, and then rebalanced to maintain red black tree property.
+    public void insert(RedBlackNode node) {
+        if(node == null) 
+            throw new IllegalArgumentException("Error: Invalid input. Cannot insert null.");
+
+        RedBlackNode xNode = root, yNode = nil;
+
+        while(xNode != nil) {
+            yNode = xNode;
+            xNode = (node.getBuildingNumber() < xNode.getBuildingNumber()) ? xNode.getLeft() : xNode.getRight();
+        }
+
+        node.setParent(yNode);
+        
+        if(yNode == nil) {
+            root = node;
+        } else if(node.getBuildingNumber() < yNode.getBuildingNumber()) {
+            yNode.setLeft(node);
+        } else if(node.getBuildingNumber() > yNode.getBuildingNumber()) {
+            yNode.setRight(node);
+        } else {
+            throw new IllegalArgumentException("Error: Trying to insert duplicate node. Building with building id " 
+            + node.getBuildingNumber() + " already exists.");
+        }
+
+        insertionRebalance(node);
+    }
+
+    // Re-establishes the RBT property if there are two consecutive red nodes after insertion.
+    private void insertionRebalance(RedBlackNode node) {
+        RedBlackNode yNode = null;
+
+        while(node.getParent().getColor() == NodeColor.RED) {
+            if(isLeftChild(node.getParent())) {
+                yNode = node.getParent().getParent().getRight();
+                if(yNode.getColor() == NodeColor.RED) {
+                    node.getParent().setColor(NodeColor.BLACK);
+                    yNode.setColor(NodeColor.BLACK);
+                    node.getParent().getParent().setColor(NodeColor.RED);
+                    node = node.getParent().getParent();
+                } else {
+                    if(isRightChild(node)) {
+                        node = node.getParent();
+                        rotateLeft(node);
+                    }
+
+                    node.getParent().setColor(NodeColor.BLACK);
+                    node.getParent().getParent().setColor(NodeColor.RED);
+                    rotateRight(node.getParent().getParent());
+                }
+            }
+            else {
+                yNode = node.getParent().getParent().getLeft();
+                if(yNode.getColor() == NodeColor.RED) {
+                    node.getParent().setColor(NodeColor.BLACK);
+                    yNode.setColor(NodeColor.BLACK);
+                    node.getParent().getParent().setColor(NodeColor.RED);
+                    node = node.getParent().getParent();
+                } else {
+                    if(isLeftChild(node)) {
+                        node = node.getParent();
+                        rotateRight(node);
+                    }
+
+                    node.getParent().setColor(NodeColor.BLACK);
+                    node.getParent().getParent().setColor(NodeColor.RED);
+                    rotateLeft(node.getParent().getParent());
+                }
+            }
+        }
+
+        root.setColor(NodeColor.BLACK);
+    }
+
+    public void delete(RedBlackNode node) {
+        if(node == null) 
+            throw new IllegalArgumentException("Error: Invalid input. Cannot remove null.");
+        
+        RedBlackNode xNode = null, yNode = node;
+        NodeColor nodeColor = node.getColor();
+
+        if(node.getLeft() == nil) {
+            xNode = node.getRight();
+            updateParentChildLink(node.getParent(), node, node.getRight());
+        } else if(node.getRight() == nil) {
+            xNode = node.getLeft();
+            updateParentChildLink(node.getParent(), node, node.getLeft());
+        } else {
+            yNode = getMaximumNode(node.getLeft());
+            nodeColor = yNode.getColor();
+
+            xNode = yNode.getRight();
+
+            if(yNode.getParent() == node) {
+                xNode.setParent(yNode);
+            } else {
+                updateParentChildLink(yNode.getParent(), yNode, yNode.getRight());
+                yNode.setRight(node.getRight());
+                yNode.getRight().setParent(yNode);
+            }
+
+            updateParentChildLink(node.getParent(), node, yNode);
+            yNode.setLeft(node.getLeft());
+            yNode.getLeft().setParent(yNode);
+            yNode.setColor(node.getColor());
+        }
+
+        if(nodeColor == NodeColor.BLACK) {
+            deletionRebalance(xNode);
         }
     }
 
-    public void print() {
-        StringBuilder sb = new StringBuilder("[");
+    // Max Node in any BST can be found by following right child pointers from root until we encounter NULL.
+    // This method returns the node with max value in the subtree rooted at node.
+    private RedBlackNode getMaximumNode(RedBlackNode node) {
+        while(node.getRight() != nil) {
+            node = node.getRight();
+        }
+        return node;
+    }
 
-        RedBlackNode copy = root;
-        Stack<RedBlackNode> stack = new Stack<>();
-        while(copy != null || !stack.isEmpty()) {
-            while(copy != null) {
-                stack.push(copy);
-                copy = copy.getLeft();
+    // Re-establishes the RBT property if a black node is deleted.
+    private void deletionRebalance(RedBlackNode node) {
+        while(node != root && node.getColor() == NodeColor.BLACK) {
+            RedBlackNode sibling;
+            if(isLeftChild(node)) {
+                sibling = node.getParent().getRight();
+                if(sibling.getColor() == NodeColor.RED) {
+                    sibling.setColor(NodeColor.BLACK);
+                    node.getParent().setColor(NodeColor.RED);
+                    rotateLeft(node.getParent());
+                    sibling = node.getParent().getRight();
+                }
+
+                if(sibling.getLeft().getColor() == NodeColor.BLACK && sibling.getRight().getColor() == NodeColor.BLACK) {
+                    sibling.setColor(NodeColor.RED);
+                    node = node.getParent();
+                } else {
+                    if(sibling.getRight().getColor() == NodeColor.BLACK) {
+                        sibling.getLeft().setColor(NodeColor.BLACK);
+                        sibling.setColor(NodeColor.RED);
+                        rotateRight(sibling);
+                        sibling = node.getParent().getRight();
+                    }
+    
+                    sibling.setColor(node.getParent().getColor());
+                    node.getParent().setColor(NodeColor.BLACK);
+                    sibling.getRight().setColor(NodeColor.BLACK);
+                    rotateLeft(node.getParent());
+                    node = root;
+                }
             }
+            else {
+                sibling = node.getParent().getLeft();
+                if(sibling.getColor() == NodeColor.RED) {
+                    sibling.setColor(NodeColor.BLACK);
+                    node.getParent().setColor(NodeColor.RED);
+                    rotateRight(node.getParent());
+                    sibling = node.getParent().getLeft();
+                }
 
-            copy = stack.pop();
-            sb.append(" [Value: " + copy.getBuildingNumber() + ", Color: " + copy.getColor().toString() + "], ");
-            copy = copy.getRight();
+                if(sibling.getRight().getColor() == NodeColor.BLACK && sibling.getLeft().getColor() == NodeColor.BLACK) {
+                    sibling.setColor(NodeColor.RED);
+                    node = node.getParent();
+                } else {
+                    if(sibling.getLeft().getColor() == NodeColor.BLACK) {
+                        sibling.getRight().setColor(NodeColor.BLACK);
+                        sibling.setColor(NodeColor.RED);
+                        rotateLeft(sibling);
+                        sibling = node.getParent().getLeft();
+                    }
+
+                    sibling.setColor(node.getParent().getColor());
+                    node.getParent().setColor(NodeColor.BLACK);
+                    sibling.getLeft().setColor(NodeColor.BLACK);
+                    rotateRight(node.getParent());
+                    node = root;
+                }
+            }
         }
 
-        sb.append("]");
+        node.setColor(NodeColor.BLACK);
+    }
 
-        System.out.println(sb.toString());
+    public RedBlackNode getRoot() {
+        return root;
+    }
+
+    public void print(RedBlackNode node) {
+        if(node == nil) return;
+        print(node.getLeft());
+        System.out.println(node.toString());
+        print(node.getRight());
     }
 }
--- src/pojo/RedBlackNode.java
+++ src/pojo/RedBlackNode.java
@@ -1,15 +1,26 @@
 package pojo;
 
 public class RedBlackNode extends Building {
+    // Null external black node.
+    public static final RedBlackNode NIL = new RedBlackNode(-1, -1, -1);
+
+    // Left, Right and Parent pointers.
+    private RedBlackNode left, right, parent;
+
+    // Color of the node. Red or Black.
     private NodeColor color;
 
+    // Corresponding heap node reference.
     private HeapNode heapNodeReference;
-
-    private RedBlackNode left, right, parent;
 
     public RedBlackNode(int buildingNumber, int executedTime, int totalTime) {
         super(buildingNumber, executedTime, totalTime);
-        this.setColor(NodeColor.RED);
+        this.setLeft(NIL);
+        this.setRight(NIL);
+        this.setParent(NIL);
+
+        // Set RED color to all the nodes except NIL node, by default.
+        this.setColor(buildingNumber == -1 ? NodeColor.BLACK : NodeColor.RED);
     }
 
     public NodeColor getColor() {
@@ -50,5 +61,10 @@ public class RedBlackNode extends Building {
 
     public void setParent(RedBlackNode parent) {
         this.parent = parent;
+    }
+
+    @Override
+    public String toString() {
+        return this.getBuildingNumber() + "," + this.color.toString();
     }
 }
